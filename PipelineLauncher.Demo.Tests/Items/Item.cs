12498e4c64fdf307404eef3c0bb17f0b4b1c1805
Leonid Abasov <leonid.abasov@namecheap.com>
2020-02-10 18:14:16 +0200

    Add project files.

---
+++ PipelineLauncher.Demo.Tests/Items/Item.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PipelineLauncher.Demo.Tests.Items
+{
+    public class Item
+    {
+        private readonly List<(int ProcessId, string StageName)> _processedBy;
+        public int Index { get; }
+
+        public Item(int index)
+        {
+            Index = index;
+            _processedBy = new List<(int ProcessId, string StageName)>();
+        }
+
+        public Item(int index, List<(int ProcessId, string StageName)> processedBy)
+        {
+            Index = index;
+            _processedBy = processedBy;
+        }
+
+        public void Process(int processId, string stageName)
+        {
+            _processedBy.Add((processId, stageName));
+        }
+
+        public override string ToString()
+        {
+            var values = _processedBy.Select(x => $"{x.StageName}:{x.ProcessId}").ToArray();
+            return $"{nameof(Item)}#{Index}: '{{{string.Join("} -> {", values)}}}';";
+        }
+    }
+}
---
+++ PipelineLauncher.Demo.Tests/Stages/Single/Stage1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using PipelineLauncher.Abstractions.PipelineStage.Configurations;
+using PipelineLauncher.Demo.Tests.Items;
+using PipelineLauncher.Stages;
+
+namespace PipelineLauncher.Demo.Tests.Stages.Single
+{
+    public class Stage1 : Stage<Item>
+    {
+        public override Item Execute(Item item)
+        {
+            if (item.Index == 1)
+            {
+                throw new Exception("fdfdf");
+            }
+
+            item.Value = item.Value + "AsyncStage1->";
+            Thread.Sleep(1000);
+
+            item.ProcessedBy.Add(Thread.CurrentThread.ManagedThreadId);
+
+            return (item);
+        }
+
+        public override String ToString()
+        {
+            return "AsyncStage1";
+        }
+    }
+
+    public class Stage2 : Stage<Item>
+    {
+        public Stage2()
+        {
+
+        }
+
+        public override Item Execute(Item item)
+        {
+            //return Remove(item);
+
+            item.Value = item.Value + "AsyncStage2->";
+            Thread.Sleep(1000);
+
+            item.ProcessedBy.Add(Thread.CurrentThread.ManagedThreadId);
+
+            return item;
+
+            //return "";
+        }
+
+
+        public bool Condition(Item input)
+        {
+            return input.Value.StartsWith("Item#0") || input.Value.StartsWith("Item#1");
+        }
+
+        public override String ToString()
+        {
+            return "AsyncStage2";
+        }
+    }
+
+    public class Stage2Alternative : Stage<Item>
+    {
+        public override Item Execute(Item item)
+        {
+            item.Value = item.Value + "AsyncStage2Alternative->";
+            Thread.Sleep(1000);
+
+            item.ProcessedBy.Add(Thread.CurrentThread.ManagedThreadId);
+
+            if (item.Value == "Item#0->AsyncStage1->AsyncStage2Alternative->")
+            {
+                //throw new Exception("dddddd");
+            }
+
+            return item;
+        }
+
+        public override StageConfiguration Configuration => new StageConfiguration { MaxDegreeOfParallelism = 2 };
+
+        public bool Condition(Item input)
+        {
+            return !input.Value.StartsWith("Item#0") && !input.Value.StartsWith("Item#1");
+        }
+
+        public override String ToString()
+        {
+            return "AsyncStage2Alternative";
+        }
+    }
+
+    public class Stage3 : Stage<Item>
+    {
+        public override Item Execute(Item item)
+        {
+            item.Value = item.Value + "AsyncStage3->";
+            Thread.Sleep(1000);
+
+            item.ProcessedBy.Add(Thread.CurrentThread.ManagedThreadId);
+
+            return item;
+        }
+
+        public override string ToString()
+        {
+            return "AsyncStage3";
+        }
+    }
+
+    public class Stage4 : Stage<Item>
+    {
+
+        public Stage4()
+        {
+
+        }
+
+        public override async Task<Item> ExecuteAsync(Item item)
+        {
+            item.Value = item.Value + "AsyncStage4->";
+            await Task.Delay(1000);
+
+            item.ProcessedBy.Add(Thread.CurrentThread.ManagedThreadId);
+
+            return item;
+        }
+
+        public override string ToString()
+        {
+            return "AsyncStage4";
+        }
+    }
+
+    public class BulkcStage_Item_To_String : BulkStage<Item, string>
+    {
+        public override IEnumerable<string> Execute(IEnumerable<Item> items)
+        {
+            foreach (var item in items)
+            {
+                item.Value = item.Value + "Stage_Item_To_String->";
+                Thread.Sleep(1000);
+
+                item.ProcessedBy.Add(Thread.CurrentThread.ManagedThreadId);
+            }
+
+            return items.Select(e => e.Value);
+        }
+
+        public override string ToString()
+        {
+            return "Stage_Item_To_String";
+        }
+    }
+
+    public class AsyncStage_String_To_Object : BulkStage<string, object>
+    {
+        public override IEnumerable<object> Execute(IEnumerable<string> items)
+        {
+            return items.Select(e => new object());
+        }
+
+        public override string ToString()
+        {
+            return "Stage_String_To_Object";
+        }
+    }
+}
