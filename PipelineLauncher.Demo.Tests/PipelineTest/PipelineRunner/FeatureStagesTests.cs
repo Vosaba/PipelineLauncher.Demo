1b7b3987b5b0a56169983b5d68f007f3a18ffbac
Leonid Abasov <leonid.abasov@gmail.com>
2020-02-24 10:35:55 +0200

    feat: separated

--- PipelineLauncher.Demo.Tests/PipelineTest/PipelineRunner/FeatureStagesTests.cs
+++ PipelineLauncher.Demo.Tests/PipelineTest/PipelineRunner/FeatureStagesTests.cs
@@ -1,61 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using PipelineLauncher.Abstractions.PipelineEvents;
-using PipelineLauncher.Demo.Tests.Extensions;
+﻿using PipelineLauncher.Demo.Tests.Extensions;
 using PipelineLauncher.Demo.Tests.Items;
 using PipelineLauncher.Demo.Tests.Stages.Single;
 using PipelineLauncher.Exceptions;
+using System;
+using System.Collections.Generic;
 using Xunit;
 using Xunit.Abstractions;
 
-namespace PipelineLauncher.Demo.Tests.PipelineTest.NonAwaitablePipelineRunner
+namespace PipelineLauncher.Demo.Tests.PipelineTest.PipelineRunner
 {
 
-    public class FeatureStagesTests : NonAwaitableTestBase
+    public class FeatureStagesTests : PipelineTestBase
     {
         public FeatureStagesTests(ITestOutputHelper output) : base(output) { }
-
-        [Fact]
-        public void Feature_Exception_Retry_Running_Stages()
-        {
-            // Test input 6 items
-            List<Item> items = MakeItemsInput(6);
-
-            // Error ocurred count
-            var errorsCount = 0;
-
-            // Configure stages
-            var pipelineSetup = PipelineCreator
-                .Stage<Stage, Item>()
-                .Stage(item =>
-                {
-                    item.Process(GetType());
-
-                    if (item.Index == 2 && errorsCount++ < 3)
-                    {
-                        throw new Exception($"{item.Name} throw exception: #'{errorsCount}'");
-                    }
-
-                    return item;
-                })
-                .Stage<Stage_1>();
-
-            // Make pipeline from stageSetup
-            var pipelineRunner = pipelineSetup.Create();
-
-            pipelineRunner.ExceptionItemsReceivedEvent += (ExceptionItemsEventArgs args) =>
-            {
-                var item = args.Items[0];
-
-                WriteSeparator();
-                WriteLine($"{item} with exception {args.Exception.Message}");
-                WriteSeparator();
-
-                args.Retry();
-            };
-
-            PostItemsAndPrintProcessedWithDefaultConditionToStop(pipelineRunner, items);
-        }
 
         [Fact]
         public void Feature_Exception_Retry_Awaitable_Stages_WrongConfiguration()
@@ -82,7 +39,7 @@ namespace PipelineLauncher.Demo.Tests.PipelineTest.NonAwaitablePipelineRunner
             // Make pipeline from stageSetup
             var pipelineRunner = pipelineSetup.CreateAwaitable();
 
-            pipelineRunner.ExceptionItemsReceivedEvent += (ExceptionItemsEventArgs args) =>
+            pipelineRunner.ExceptionItemsReceivedEvent += (args) =>
             {
                 var item = args.Items[0];
 
@@ -124,8 +81,8 @@ namespace PipelineLauncher.Demo.Tests.PipelineTest.NonAwaitablePipelineRunner
 
             // Make pipeline from stageSetup
             var pipelineRunner = pipelineSetup.CreateAwaitable();
-                
-            pipelineRunner.ExceptionItemsReceivedEvent += (ExceptionItemsEventArgs args) =>
+
+            pipelineRunner.ExceptionItemsReceivedEvent += (args) =>
             {
                 var item = args.Items[0];
 
